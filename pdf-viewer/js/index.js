12a6fbbe8c08369d536a03d293b571b7f5f65ce8
Alexandre do Vale <vale.apv@gmail.com>
2019-02-21 21:10:45 -0300

    Funções e eventos do pdfJs

---
+++ pdf-viewer/js/index.js
@@ -0,0 +1,85 @@
+const url = "../docs/cv.pdf";
+// const url = "https://github.com/alexandrevale/js-101/blob/master/pdf-viewer/docs/cv.pdf";
+
+// Config
+let pdfDoc = null,
+	pageNum = 1,
+	pageIsRendering = false,
+	pageNumIsPending = null;
+
+const scale = 1.5,
+	canvas = document.querySelector('#pdf-render'),
+	ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+	pageIsRendering = true;
+
+	// Get page
+	pdfDoc.getPage(num).then(page => {
+		// Set scale
+		const viewport = page.getViewport({ scale });
+		canvas.width = viewport.width;
+		canvas.height = viewport.height;
+
+		const renderCtx = {
+			canvasContext: ctx,
+			viewport
+		}
+
+		page.render(renderCtx).promise.then(() => {
+			pageIsRendering = false;
+
+			if(pageNumIsPending !== null) {
+				renderPage(pageNumIsPending);
+				pageNumIsPending = null;
+			}
+		});
+
+		// Output current page
+		document.querySelector('#page-num').textContext = num;
+	});
+}
+
+// Check for pages rendering
+const queueRenderPage = num => {
+	if (pageNumIsPending) {
+		pageNumIsPending = num;
+	} else {
+		renderPage(num)
+	}
+}
+
+// Show prev page
+const showPrevPage = () => {
+	if (pageNum <= 1) {
+		return;
+	}
+
+	pageNum--;
+	queueRenderPage(pageNum);
+}
+
+// Show next page
+const showNextPage = () => {
+	if (pageNum >= pdfDoc.numPages) {
+		return;
+	}
+
+	pageNum++;
+	queueRenderPage(pageNum);
+}
+
+// Get doc
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+	pdfDoc = pdfDoc_;
+	// console.log(pdfDoc);
+	
+	document.querySelector('#page-count').textContent = pdfDoc.numPages/
+
+	renderPage(pageNum);
+});
+
+// btn events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
